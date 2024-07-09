d8c4703d03de3a89775e88333aa7bf125380dc65
Jesús Villar <jesusvillarjimenez@gmail.com>
2024-07-09 09:57:22 +0200

    prettier config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), mdx(), sitemap()]
+  integrations: [tailwind(), icon(), mdx(), sitemap()],
 });
