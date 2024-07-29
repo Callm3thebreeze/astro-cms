276b63e37cb981220be62e9c3a9a532f799c8311
Jesús Villar <jesusvillarjimenez@gmail.com>
2024-07-29 21:04:31 +0200

    visual fix

--- src/components/controlPanel/featuresForm/FeaturesFormContainer.jsx
+++ src/components/controlPanel/featuresForm/FeaturesFormContainer.jsx
@@ -51,103 +51,104 @@ const FeaturesFormContainer = ({
   return (
     <div>
       <form onSubmit={handleSave}>
-        <div className="space-y-4 md:col-span-1 bg-gray-100 rounded-md p-4">
-          <legend className="text-lg font-bold text-gray-900 mb-4">
-            Features
-          </legend>
-          <div>
-            <label
-              htmlFor="featureTitle"
-              className="block text-sm font-medium text-gray-700">
-              Título
-            </label>
-            <input
-              type="text"
-              name="featureTitle"
-              id="featureTitle"
-              value={form.featureTitle || ""}
-              onChange={(e) => handleInputChange(e, setForm)}
-              className="mt-3 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="featureSubtitle"
-              className="block text-sm font-medium text-gray-700">
-              Subtítulo
-            </label>
-            <textarea
-              name="featureSubtitle"
-              id="featureSubtitle"
-              value={form.featureSubtitle || ""}
-              onChange={(e) => handleInputChange(e, setForm)}
-              className="mt-3 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md resize-none min-h-[8rem] max-h-[12rem]"></textarea>
-          </div>
+        <div>
+          <div className="space-y-4 md:col-span-1 bg-gray-100 rounded-md p-4">
+            <legend className="text-lg font-bold text-gray-900 mb-4">
+              Features
+            </legend>
+            <div>
+              <label
+                htmlFor="featureTitle"
+                className="block text-sm font-medium text-gray-700">
+                Título
+              </label>
+              <input
+                type="text"
+                name="featureTitle"
+                id="featureTitle"
+                value={form.featureTitle || ""}
+                onChange={(e) => handleInputChange(e, setForm)}
+                className="mt-3 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="featureSubtitle"
+                className="block text-sm font-medium text-gray-700">
+                Subtítulo
+              </label>
+              <textarea
+                name="featureSubtitle"
+                id="featureSubtitle"
+                value={form.featureSubtitle || ""}
+                onChange={(e) => handleInputChange(e, setForm)}
+                className="mt-3 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md resize-none min-h-[8rem] max-h-[12rem]"></textarea>
+            </div>
 
-          {items.map((item, index) => (
-            <div key={index} className="space-y-4 border-b pb-4">
-              <div>
-                <label
-                  htmlFor={`item${index + 1}title`}
-                  className="block text-sm font-medium text-gray-700">
-                  Título del Item
-                </label>
-                <input
-                  type="text"
-                  name="title"
-                  id={`item${index + 1}title`}
-                  value={item.title}
-                  onChange={(e) => handleItemChange(index, e)}
-                  className="mt-3 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                />
+            {items.map((item, index) => (
+              <div key={index} className="space-y-4 border-b pb-4">
+                <div>
+                  <label
+                    htmlFor={`item${index + 1}title`}
+                    className="block text-sm font-medium text-gray-700">
+                    Título del Item
+                  </label>
+                  <input
+                    type="text"
+                    name="title"
+                    id={`item${index + 1}title`}
+                    value={item.title}
+                    onChange={(e) => handleItemChange(index, e)}
+                    className="mt-3 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor={`item${index + 1}description`}
+                    className="block text-sm font-medium text-gray-700">
+                    Descripción del Item
+                  </label>
+                  <textarea
+                    name="description"
+                    id={`item${index + 1}description`}
+                    value={item.description}
+                    onChange={(e) => handleItemChange(index, e)}
+                    className="mt-3 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md resize-none"
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor={`item${index + 1}icon`}
+                    className="block text-sm font-medium text-gray-700">
+                    Icono del Item
+                  </label>
+                  <input
+                    type="text"
+                    name="icon"
+                    id={`item${index + 1}icon`}
+                    value={item.icon}
+                    onChange={(e) => handleItemChange(index, e)}
+                    className="mt-3 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                  />
+                </div>
+                <button
+                  type="button"
+                  onClick={() => handleRemoveItem(index)}
+                  className="mt-2 py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
+                  Eliminar Item
+                </button>
               </div>
-              <div>
-                <label
-                  htmlFor={`item${index + 1}description`}
-                  className="block text-sm font-medium text-gray-700">
-                  Descripción del Item
-                </label>
-                <textarea
-                  name="description"
-                  id={`item${index + 1}description`}
-                  value={item.description}
-                  onChange={(e) => handleItemChange(index, e)}
-                  className="mt-3 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md resize-none"
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor={`item${index + 1}icon`}
-                  className="block text-sm font-medium text-gray-700">
-                  Icono del Item
-                </label>
-                <input
-                  type="text"
-                  name="icon"
-                  id={`item${index + 1}icon`}
-                  value={item.icon}
-                  onChange={(e) => handleItemChange(index, e)}
-                  className="mt-3 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                />
-              </div>
+            ))}
+
+            {items.length < 6 && (
               <button
                 type="button"
-                onClick={() => handleRemoveItem(index)}
-                className="mt-2 py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
-                Eliminar Item
+                onClick={handleAddItem}
+                className="mt-2 py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                Añadir Item
               </button>
-            </div>
-          ))}
-
-          {items.length < 6 && (
-            <button
-              type="button"
-              onClick={handleAddItem}
-              className="mt-2 py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-              Añadir Item
-            </button>
-          )}
-
+            )}
+          </div>
           <div className="md:col-span-2 flex justify-end">
             <button
               type="submit"
